4eacbc5f91a9c2a7cf959fb61e93ce945b5b7494
Andres Paiva <andrespaivasilveiraa@gmail.com>
2026-02-08 00:19:02 -0300

    routes with attributes support for methods and csrf

--- set-lang/index.php
+++ set-lang/index.php
@@ -1,12 +1,19 @@
 <?php
- /** @var \Core\App $app */
+
+/** @var \Core\App $app */
 include_once "../app/index.php";
 
-$app->get(callback: function ($req, $res) use ($app) {
+use Core\GET;
+
+#[GET]
+function setLang($req, $res)
+{
+    global $app;
     $newLang = $_GET["lang"] ?? $app->getLangDefault();
     $app->setSession(key: "lang", value: $newLang);
     $back = $_SERVER['HTTP_REFERER'] ?? '/';
     $app->redirect($back);
-});
+}
+$app->add(callback: 'setLang');
 
 $app->run();
