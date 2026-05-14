c31cdf3b5374b1c8802ef932e1780cc181f54f5e
Andres Paiva <87265390+seip25@users.noreply.github.com>
2026-05-14 15:09:41 -0300

    Create test.php

---
+++ LilaPHP/routes/api/test.php
@@ -0,0 +1,20 @@
+<?php
+
+include_once __DIR__ . "/../../index.php";
+use Core\App;
+use Core\GET;
+use Core\Response;
+
+
+$app = new App();
+
+#[GET]
+function api($req, Response $res)
+{
+    return $res->json([
+        "message" => "Welcome to the API Test in LilaPHP"
+    ]);
+}
+
+$app->add(callback: 'api');
+$app->run();
